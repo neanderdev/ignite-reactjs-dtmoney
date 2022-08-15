caabf7a08722bfe4f660486b8120e3d81aba21ca
neanderdev <neander.faria@hotmail.com>
2022-08-15 13:58:47 -0300

    Estrutura visual - Tabela de transações

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,3 +1,5 @@
+import { TransactionsContainer, TransactionsTable, PriceHighlight } from './styles'
+
 import { Header } from '../../components/Header'
 import { Summary } from '../../components/Summary'
 
@@ -7,6 +9,33 @@ export function Transactions() {
             <Header />
 
             <Summary />
+
+            <TransactionsContainer>
+                <TransactionsTable>
+                    <tbody>
+                        <tr>
+                            <td width='50%'>Desenvolvimento de site</td>
+                            <td>
+                                <PriceHighlight variant='income'>
+                                    R$ 12.000,00
+                                </PriceHighlight>
+                            </td>
+                            <td>Venda</td>
+                            <td>15/08/2022</td>
+                        </tr>
+                        <tr>
+                            <td width='50%'>Pizza</td>
+                            <td>
+                                <PriceHighlight variant='outcome'>
+                                    - R$ 500,00
+                                </PriceHighlight>
+                            </td>
+                            <td>Alimentação</td>
+                            <td>15/08/2022</td>
+                        </tr>
+                    </tbody>
+                </TransactionsTable>
+            </TransactionsContainer>
         </div>
     )
 }
