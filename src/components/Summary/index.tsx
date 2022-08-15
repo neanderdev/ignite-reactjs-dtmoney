40ffdd676aadb2ede20f79b9f184ac57a0e4a83b
neanderdev <neander.faria@hotmail.com>
2022-08-15 15:59:41 -0300

    Conexão com a API - Calculando resumo de transações

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,8 +1,32 @@
+import { useContext } from 'react'
 import { ArrowCircleDown, ArrowCircleUp, CurrencyDollar } from 'phosphor-react'
+
+import { TransactionsContext } from '../../contexts/TransactionsContext'
 
 import { SummaryContainer, SummaryCard } from './styles'
 
 export function Summary() {
+    const { transactions } = useContext(TransactionsContext)
+
+    const summary = transactions.reduce(
+        (acc, transaction) => {
+            if (transaction.type === 'income') {
+                acc.income += transaction.price
+                acc.total += transaction.price
+            } else {
+                acc.outcome += transaction.price
+                acc.total += transaction.price
+            }
+
+            return acc
+        },
+        {
+            income: 0,
+            outcome: 0,
+            total: 0,
+        }
+    )
+
     return (
         <SummaryContainer>
             <SummaryCard>
@@ -12,7 +36,7 @@ export function Summary() {
                     <ArrowCircleUp size={32} color='#00b37e' />
                 </header>
 
-                <strong>R$ 17.400,00</strong>
+                <strong>{summary.income}</strong>
             </SummaryCard>
 
             <SummaryCard>
@@ -22,7 +46,7 @@ export function Summary() {
                     <ArrowCircleDown size={32} color='#f75a68' />
                 </header>
 
-                <strong>R$ 5.000,00</strong>
+                <strong>{summary.outcome}</strong>
             </SummaryCard>
 
             <SummaryCard variant='green'>
@@ -32,7 +56,7 @@ export function Summary() {
                     <CurrencyDollar size={32} color='#fff' />
                 </header>
 
-                <strong>R$ 12.400,00</strong>
+                <strong>{summary.total}</strong>
             </SummaryCard>
         </SummaryContainer>
     )
